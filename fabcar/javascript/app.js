92f832b3806fd67b868cdcf6c27116808e327c0a
nammi31 <happynews1997@gmail.com>
2019-10-29 16:00:27 +0600

    initial commit

--- fabcar/javascript/app.js
+++ fabcar/javascript/app.js
@@ -62,170 +62,48 @@ app.post('/', (req, res) => {
         humidity = data.humidity
         piID=data.piID
     }
-    // var Fabric_Client = require('fabric-client');
-    // var util = require('util');
-    // var fabric_client = new Fabric_Client();
-    // // setup the fabric network
-    // var channel = fabric_client.newChannel('mychannel');
-    // var peer = fabric_client.newPeer('grpc://localhost:7051');
-    // channel.addPeer(peer);
-    // var order = fabric_client.newOrderer('grpc://localhost:7050')
-    // channel.addOrderer(order);
-    // //
-    // var member_user = null;
-    // var store_path = path.join(__dirname, 'hfc-key-store');
-    // console.log('Store path:' + store_path);
-    // var tx_id = null;
+   // START INVOKE TX
+   sync function main() {
+    try {
 
-    // // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-    // Fabric_Client.newDefaultKeyValueStore({
-    //     path: store_path
-    // }).then((state_store) => {
-    //     // assign the store to the fabric client
-    //     fabric_client.setStateStore(state_store);
-    //     var crypto_suite = Fabric_Client.newCryptoSuite();
-    //     // use the same location for the state store (where the users' certificate are kept)
-    //     // and the crypto store (where the users' keys are kept)
-    //     var crypto_store = Fabric_Client.newCryptoKeyStore({
-    //         path: store_path
-    //     });
-    //     crypto_suite.setCryptoKeyStore(crypto_store);
-    //     fabric_client.setCryptoSuite(crypto_suite);
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
 
-    //     // get the enrolled user from persistence, this user will sign all requests
-    //     return fabric_client.getUserContext('user1', true);
-    // }).then((user_from_store) => {
-    //     if (user_from_store && user_from_store.isEnrolled()) {
-    //         console.log('Successfully loaded user1 from persistence');
-    //         member_user = user_from_store;
-    //     } else {
-    //         throw new Error('Failed to get user1.... run registerUser.js');
-    //     }
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists('user1');
+        if (!userExists) {
+            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
 
-    //     // get a transaction id object based on the current user assigned to fabric client
-    //     tx_id = fabric_client.newTransactionID();
-    //     console.log("Assigning transaction_id: ", tx_id._transaction_id);
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
 
-    //     // createCar chaincode function - requires 5 args, ex: args: ['CAR12', 'Honda', 'Accord', 'Black', 'Tom'],
-    //     // changeCarOwner chaincode function - requires 2 args , ex: args: ['CAR10', 'Dave'],
-    //     // must send the proposal to endorsing peers
-    //     var request = {
-    //         //targets: var default to the peer assigned to the client
-    //         chaincodeId: 'fabcar',
-    //         fcn: 'sensorData',
-    //         args: [temp, humidity,piID],
-    //         chainId: 'mychannel',
-    //         txId: tx_id
-    //     };
-    //     console.log("request");
-    //     console.log("--------------------");
-    //     console.log(request);
-        
-        
-        
-    //     // send the transaction proposal to the peers
-    //     return channel.sendTransactionProposal(request);
-    // }).then((results) => {
-    //     var proposalResponses = results[0];
-    //     var proposal = results[1];
-    //     var isProposalGood = false;
-    //     if (proposalResponses && proposalResponses[0].response &&
-    //         proposalResponses[0].response.status === 200) {
-    //         isProposalGood = true;
-    //         console.log('Transaction proposal was good');
-    //     } else {
-    //         console.error('Transaction proposal was bad');
-    //     }
-    //     if (isProposalGood) {
-    //         console.log(util.format(
-    //             'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
-    //             proposalResponses[0].response.status, proposalResponses[0].response.message));
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
 
-    //         // build up the request for the orderer to have the transaction committed
-    //         var request = {
-    //             proposalResponses: proposalResponses,
-    //             proposal: proposal
-    //         };
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
 
-    //         // set the transaction listener and set a timeout of 30 sec
-    //         // if the transaction did not get committed within the timeout period,
-    //         // report a TIMEOUT status
-    //         var transaction_id_string = tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
-    //         var promises = [];
+        // Submit the specified transaction.
+        await contract.submitTransaction('sensorData', temp, humidity,piID);
+        console.log('Transaction has been submitted');
 
-    //         var sendPromise = channel.sendTransaction(request);
-    //         promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
+        // Disconnect from the gateway.
+        await gateway.disconnect();
 
-    //         // get an eventhub once the fabric client has a user assigned. The user
-    //         // is required bacause the event registration must be signed
-    //         var event_hub = channel.newChannelEventHub(peer);
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
 
-    //         // using resolve the promise so that result status may be processed
-    //         // under the then clause rather than having the catch clause process
-    //         // the status
-    //         var txPromise = new Promise((resolve, reject) => {
-    //             var handle = setTimeout(() => {
-    //                 event_hub.unregisterTxEvent(transaction_id_string);
-    //                 event_hub.disconnect();
-    //                 resolve({
-    //                     event_status: 'TIMEOUT'
-    //                 }); //we could use reject(new Error('Trnasaction did not compvare within 30 seconds'));
-    //             }, 3000);
-    //             event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
-    //                     // this is the callback for transaction event status
-    //                     // first some clean up of event listener
-    //                     clearTimeout(handle);
-
-    //                     // now var the application know what happened
-    //                     var return_status = {
-    //                         event_status: code,
-    //                         tx_id: transaction_id_string
-    //                     };
-    //                     if (code !== 'VALID') {
-    //                         console.error('The transaction was invalid, code = ' + code);
-    //                         resolve(return_status); // we could use reject(new Error('Problem with the tranaction, event status ::'+code));
-    //                     } else {
-    //                         console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
-    //                         resolve(return_status);
-    //                     }
-    //                 }, (err) => {
-    //                     //this is the callback if something goes wrong with the event registration or processing
-    //                     reject(new Error('There was a problem with the eventhub ::' + err));
-    //                 }, {
-    //                     disconnect: true
-    //                 } //disconnect when compvare
-    //             );
-    //             event_hub.connect();
-
-    //         });
-    //         promises.push(txPromise);
-
-    //         return Promise.all(promises);
-    //     } else {
-    //         console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-    //         throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-    //     }
-    // }).then((results) => {
-    //     console.log('Send transaction promise and event listener promise have compvared');
-    //     // check the results in the order the promises were added to the promise all list
-    //     if (results && results[0] && results[0].status === 'SUCCESS') {
-    //         console.log('Successfully sent transaction to the orderer.');
-    //     } else {
-    //         console.error('Failed to order the transaction. Error code: ' + results[0].status);
-    //     }
-
-    //     if (results && results[1] && results[1].event_status === 'VALID') {
-    //         console.log('Successfully committed the change to the ledger by the peer');
-    //         res.sendFile(__dirname + '/index.html')
-    //         latestData();
-    //         //console.log("Response is ", JSON.stringify(results));
-    //     } else {
-    //         console.log('Transaction failed to be committed to the ledger due to ::' + results[1].event_status);
-    //     }
-    // }).catch((err) => {
-    //     console.error('Failed to invoke successfully :: ' + err);
-    // });
-
+main();
+   // END INVOKE TX
     
 })
 
@@ -371,16 +249,23 @@ app.get('/register', (req,res)=>{
 // actions after submitting username and private key in login page  
 app.post('/upload', upload.single('keyFile'), function (req, res, next) {
 
-    msg = req.body
-    console.log('msg : ', msg)
-    let buf = req.file.buffer
-    console.log(buf)
+    // private key file
+    var buf = req.file.buffer
+    console.log('keyFile buffer : ',buf, 'typeof buf : ', typeof buf)
+    console.log("typeof buf : ",typeof buf)
     console.log(" private key:", buf.toString('utf8') );
-        
-   let privkey = ursa.createPrivateKey(buf);
-    
-   let encryptedMsg = privkey.privateEncrypt(msg, 'utf8', 'base64'); 
-   console.log('encrypted message: ', encryptedMsg)
+
+    var user = {
+        userId : req.body.username
+        password : req.body.password
+        privateKey : buf.toString()
+    }
+    console.log('user : ', user)
+
+    let privkey = ursa.createPrivateKey(buf);
+    msg = req.body
+    let encryptedMsg = privkey.privateEncrypt(msg, 'utf8', 'base64'); 
+    console.log('encrypted message: ', encryptedMsg)
     // START QUERY TRANSACTION
     //load public key from Blockchain for signature verification
    async function main() {
@@ -392,16 +277,16 @@ app.post('/upload', upload.single('keyFile'), function (req, res, next) {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists(username);
+        const userExists = await wallet.exists('shoumik');
         if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('An identity for the user "shoumik" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: username, discovery: { enabled: false } });
+        await gateway.connect(ccp, { wallet, identity: 'shoumik', discovery: { enabled: false } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('mychannel');
@@ -411,9 +296,10 @@ app.post('/upload', upload.single('keyFile'), function (req, res, next) {
 
         // send request to get public key of user
         /****** have to write getPubKey() function in chaincode */
-        const result = await contract.evaluateTransaction('getUserInfo',username);
+        const result = await contract.evaluateTransaction('getUserInfo','shoumik');
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-        //pubKey = JSON.parse(result).pubKey
+        res.sendFile(__dirname + '/index.html')
+            latestData();
 
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
@@ -493,47 +379,7 @@ function latestData() {
   
   main();
     
-    //     // get the enrolled user from persistence, this user will sign all requests
-    //     return fabric_client.getUserContext('user1', true);
-    // }).then((user_from_store) => {
-    //     if (user_from_store && user_from_store.isEnrolled()) {
-    //         console.log('Successfully loaded user1 from persistence');
-    //         member_user = user_from_store;
-    //     } else {
-    //         throw new Error('Failed to get user1.... run registerUser.js');
-    //     }
-
-    //     // queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
-    //     // queryAllCars chaincode function - requires no arguments , ex: args: [''],
-    //     const request = {
-    //         //targets : --- varting this default to the peers assigned to the channel
-    //         chaincodeId: 'fabcar',
-    //         fcn: 'lastData',
-    //         args: ['']
-    //     };
-
-    //     // send the query proposal to the peer
-    //     return channel.queryByChaincode(request);
-    // }).then((query_responses) => {
-    //     console.log("Query has compvared, checking results");
-    //     // query_responses could have more than one  results if there multiple peers were used as targets
-    //     if (query_responses && query_responses.length == 1) {
-    //         if (query_responses[0] instanceof Error) {
-    //             console.error("error from query = ", query_responses[0]);
-    //         } else {
-    //             console.log("latestDataResponse is :", JSON.parse(query_responses[0]));
-    //             finalData=JSON.parse(query_responses[0])
-    //             console.log('====================================');
-    //             console.log("finalData ",finalData);
-    //             console.log('====================================');
-    //              io.emit('data',finalData)
-    //         }
-    //     } else {
-    //         console.log("No payloads were returned from query");
-    //     }
-    // }).catch((err) => {
-    //     console.error('Failed to query successfully :: ' + err);
-    // });
+  
 }
 
 
